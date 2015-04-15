8d52a5f9c20c83185a856df93224978c7bbd3579
Atsushi Tadokoro <tadokoro@gmail.com>
2015-04-15 18:09:51 +0900

    update

--- myApps/myApps.clj
+++ myApps/myApps.clj
@@ -5,9 +5,9 @@
 (t/start-fullscreen "myApps/plasma.glsl")
 (t/start-fullscreen "myApps/blobs.glsl")
 (t/start-fullscreen "myApps/noiseSphere.glsl")
-
+p
 (demo 6000 (rlpf (saw [80 81]) (+ (* (lf-noise0 0.25) [40 44]) 100)))
-(demo 30 (rlpf (saw [120 121]) (+ (* (lf-noise0 0.5) [50 54]) 110)))
+(demo 30 (rlpf (saw [120 121]) (+ (* (lf-noise2 0.5) [50 54]) 110)))
 (demo 30 (rlpf (saw [120 121]) (+ (* (lf-noise0 1) [60 64]) 30)))
 (demo 30 (rlpf (saw [120 121]) (+ (* (lf-noise0 1) [120 18]) 0)))
 (demo 30 (rlpf (saw [1200 1210]) (+ (* (lf-noise0 1) [200 220]) 0)))
